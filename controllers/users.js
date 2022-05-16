26c0eebbc3069b8b2944a72dadebf1c7eed2d47a
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2022-05-16 14:33:42 +0530

    Implement signup for user

---
+++ controllers/users.js
@@ -0,0 +1,30 @@
+import bcrypt from "bcrypt";
+import {Router} from "express";
+
+import User from "../models/user.js";
+
+const usersRouter = Router()
+
+usersRouter.post('/', async (request, response, next) => {
+	const {password, username} = request.body
+	console.log(request.body)
+
+	if (!password || password.length < 3) {
+		return response.status(400).json({error: "password must have length 3"})
+	}
+
+	const saltRounds = 10
+	const passwordHash = await bcrypt.hash(password, saltRounds)
+
+	const user = new User({
+		username,
+		passwordHash
+	})
+
+	const savedUser = await user.save()
+
+	response.json(savedUser)
+})
+
+
+export default usersRouter
