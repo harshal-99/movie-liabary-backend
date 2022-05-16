f38cffcd3a7775bd7c178515a125d59d8df5c780
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2022-05-16 14:37:46 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import http from "http"
+
+import app from "./app.js";
+import {PORT} from "./utils/config.js";
+
+
+const server = http.createServer(app)
+
+server.listen(PORT, () => {
+	console.log(`Server running on PORT ${PORT}`)
+})
